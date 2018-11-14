1b6cd11e48548d8b5f847ed5d3643d0dcf2db739
Katy Deng <deng2017.xq@gmail.com>
2018-11-14 14:56:34 -0800

    completed custom components

--- App.js
+++ App.js
@@ -7,25 +7,20 @@ import {
   Button,
   TouchableHighlight
 } from "react-native";
-import ListItem from './src/components/ListItem'
+import NotesInput from './src/components/NotesInput'
+import NotesList from "./src/components/NotesList";
+
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userInput: "",
+    state = {
       notes: []
     };
-  }
 
-  handleAddNote = () => {
-    if (this.state.userInput === "") {
-      return;
-    }
+  handleAddNote = (userInputNote) => {
+ 
     this.setState(prevState => {
-      return {
-        notes: prevState.notes.concat(prevState.userInput),
-        userInput: ""
+      return { 
+        notes: prevState.notes.concat(userInputNote) 
       };
     });
   };
@@ -37,39 +32,13 @@ export default class App extends Component {
     });
   };
   render() {
-    // const outputNote = this.state.notes.map((item, i) => (
-    //   <View key={i} style={styles.outputContainer}>
-    //     <Text>{item}</Text>
-    //     <Button title="delete" onPress={() => this.handleDeleteButton(i)} />
-    //   </View>
-    // ));
-    const outputNote = this.state.notes.map((note,i) => (
-        <ListItem key={i} notesItem = {note} />
-    ));
-    return (
-      <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="> note"
-            value={this.state.userInput}
-            onChangeText={userInput => {
-              this.setState({ userInput });
-            }}
-            style={styles.inputArea}
-          />
-          <TouchableHighlight
-            style={styles.button}
-            onPress={this.handleAddNote}
-            underlayColor="gray"
-          >
-            <View style={styles.button}>
-              <Text style={styles.buttonText}>+</Text>
-            </View>
-          </TouchableHighlight>
-        </View>
-        <View style ={styles.outputContainer}>{outputNote}</View>
-      </View>
-    );
+  
+  
+    return <View style={styles.container}>
+        <NotesInput onPressAddNote={this.handleAddNote} />
+        <NotesList notes={this.state.notes} onPress={this.handleDeleteButton} />
+      </View>;
+    
   }
 }
 
@@ -81,37 +50,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#F5FCFF",
     paddingTop: 40
   },
-  inputContainer: {
-    width: "90%",
-    // backgroundColor:'#4f52',
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  inputArea: {
-    width: "80%",
-    backgroundColor: "lightgray",
-    height: 60
-  },
-  button: {
-    backgroundColor: "pink",
-    width: 60,
-    height: 60,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 100
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 40,
-    alignItems: "center"
-  },
-  outputContainer: {
-    width: "90%",
-    backgroundColor: "pink",
-    // flexDirection: "row",
-    // justifyContent: "space-between",
-    // alignItems: "center",
-    margin: 5
-  }
+
+
 });
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
 const listItem = (props)=>
  <View style = {styles.listItem}>  
     <Text>{props.notesItem}</Text> 
+    <Button title='Delete' onPress={props.onDeleteButtonPress} />
  </View> 
 
 const styles = StyleSheet.create({
   listItem: {
     width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
     padding: 10,
-    marginBottom:5,
-    backgroundColor: '#eee'
+    marginBottom: 5,
+    backgroundColor: "#eee"
   }
-})
+});
 export default listItem
