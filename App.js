82b35c43b1ef25cd03007cfbd415431e05a28359
Katy Deng <deng2017.xq@gmail.com>
2018-11-14 13:23:21 -0800

    custom components

--- App.js
+++ App.js
@@ -1,65 +1,73 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight
+} from "react-native";
+import ListItem from './src/components/ListItem'
 
-
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, TouchableHighlight} from 'react-native';
-
-
-export default class App extends Component{
-  constructor(props){
-    super(props)
+export default class App extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      userInput: '',
+      userInput: "",
       notes: []
-    }
+    };
   }
 
-  handleAddNote = ()=>{
-    if(this.state.userInput === ''){
+  handleAddNote = () => {
+    if (this.state.userInput === "") {
       return;
     }
-    this.setState((prevState) =>{
-      return{
+    this.setState(prevState => {
+      return {
         notes: prevState.notes.concat(prevState.userInput),
-        userInput: '',
-      }
-    })
-  }
-  handleDeleteButton =(i)=>{
-    let newNotes = this.state.notes
-    newNotes.splice(i,1); 
-    // alert(`your still have notes: ${newNotes}`)
-
+        userInput: ""
+      };
+    });
+  };
+  handleDeleteButton = i => {
+    let newNotes = this.state.notes;
+    newNotes.splice(i, 1);
     this.setState({
       notes: newNotes
-    })
-
-  }
+    });
+  };
   render() {
-    const outputNote = this.state.notes.map((item, i)=>(
-      <View key={i} style={styles.outputContainer}> 
-        <Text>{item}</Text>
-        <Button title ="delete" onPress = {()=>this.handleDeleteButton(i)}/>
-      </View>
-    )
-  )
+    // const outputNote = this.state.notes.map((item, i) => (
+    //   <View key={i} style={styles.outputContainer}>
+    //     <Text>{item}</Text>
+    //     <Button title="delete" onPress={() => this.handleDeleteButton(i)} />
+    //   </View>
+    // ));
+    const outputNote = this.state.notes.map((note,i) => (
+        <ListItem key={i} notesItem = {note} />
+    ));
     return (
       <View style={styles.container}>
         <View style={styles.inputContainer}>
-            <TextInput 
-                placeholder = '> note'
-                value = {this.state.userInput}
-                onChangeText={(userInput)=>{
-                  this.setState({userInput})
-                }}
-                style={styles.inputArea}
-            />
-            <TouchableHighlight  style={styles.button} onPress = {this.handleAddNote}  underlayColor ='gray'> 
-              <View style={styles.button}> 
-                <Text style={styles.buttonText}>+</Text>
-              </View>
-            </TouchableHighlight>
+          <TextInput
+            placeholder="> note"
+            value={this.state.userInput}
+            onChangeText={userInput => {
+              this.setState({ userInput });
+            }}
+            style={styles.inputArea}
+          />
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this.handleAddNote}
+            underlayColor="gray"
+          >
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>+</Text>
+            </View>
+          </TouchableHighlight>
         </View>
-       {outputNote}
+        <View style ={styles.outputContainer}>{outputNote}</View>
       </View>
     );
   }
@@ -68,47 +76,42 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container: {
     // flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    paddingTop: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    paddingTop: 40
   },
-  inputContainer:{
-    width:'90%',
+  inputContainer: {
+    width: "90%",
     // backgroundColor:'#4f52',
-    flexDirection: 'row',
+    flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: 'center'
+    alignItems: "center"
   },
-  inputArea:{
-    width:'80%',
-    backgroundColor: 'lightgray',
+  inputArea: {
+    width: "80%",
+    backgroundColor: "lightgray",
     height: 60
   },
-  button:{
-    backgroundColor: 'pink',
+  button: {
+    backgroundColor: "pink",
     width: 60,
     height: 60,
-    alignItems: 'center',
-    justifyContent:'center',
-    borderRadius: 100,
-
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100
   },
-  buttonText:{
-    color:'white',
+  buttonText: {
+    color: "white",
     fontSize: 40,
-    alignItems: 'center',
-
+    alignItems: "center"
   },
   outputContainer: {
-    width:'90%',
-    backgroundColor:'#4f52',
-    flexDirection: 'row',
-    justifyContent: "space-between",
-    alignItems: 'center',
-    margin:10 
+    width: "90%",
+    backgroundColor: "pink",
+    // flexDirection: "row",
+    // justifyContent: "space-between",
+    // alignItems: "center",
+    margin: 5
   }
- 
- 
- 
 });
